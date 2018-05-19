12360f3311517e4908a24f77d136fa515170e730
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-05-19 18:32:29 +0300

    1. Создание вспомогательного класса. 2. Использование созданного класса для передачи тестовых данных в соззданные ранее вспомогательные методы.

---
+++ contact_class.py
@@ -0,0 +1,20 @@
+class contact_form:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone,
+                 mobile_phone, work_phone, email, homepage, option_day_birthday, option_month_birthday,
+                 year_birthday, home_address):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.email = email
+        self.homepage = homepage
+        self.option_day_birthday = option_day_birthday
+        self.option_month_birthday = option_month_birthday
+        self.year_birthday = year_birthday
+        self.home_address = home_address
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from contact_class import contact_form
 
 
 def is_alert_present(wd):
@@ -19,98 +20,96 @@ class test_add_contact(unittest.TestCase):
     def test_test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.create_new_contact(wd, firstname="Александр", middlename="Владимирович", lastname="Смоляк", nickname="Crucis",
+        self.login(wd, username="admin", password="secret")
+        self.create_new_contact(wd, contact_form(firstname="Александр", middlename="Владимирович", lastname="Смоляк", nickname="Crucis",
                                 title="Новый контакт", company="Новая компания", address="Адрес компании",
                                 home_phone="+7(812)999-99-99", mobile_phone="+7(911)923-00-99", work_phone="+7(812)777-77-77",
                                 email="crucis.spb@gmail.com", homepage="http://site.ru/",
                                 option_day_birthday="//div[@id='content']/form/select[1]//option[12]",
                                 option_month_birthday="//div[@id='content']/form/select[2]//option[7]", year_birthday="1983",
-                                home_address="Домашний адрес")
+                                home_address="Домашний адрес"))
         self.logout(wd)
 
     def logout(self, wd):
         # Логаут
         wd.find_element_by_link_text("Logout").click()
 
-    def create_new_contact(self, wd, firstname, middlename, lastname, nickname, title, company, address, home_phone,
-                           mobile_phone, work_phone, email, homepage, option_day_birthday, option_month_birthday,
-                           year_birthday, home_address):
+    def create_new_contact(self, wd, contact_form):
         # Добавление нового контакта
         wd.find_element_by_link_text("add new").click()
         # Заполнение поля "Имя"
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact_form.firstname)
         # Заполнение поля "Отчество"
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(contact_form.middlename)
         # Заполнение поля "Фамилия"
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact_form.lastname)
         # Заполнение поля "Псевдоним"
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(contact_form.nickname)
         # Заполнение поля "Заголовок"
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(contact_form.title)
         # Заполнение поля "Компания"
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(contact_form.company)
         # Заполнение поля "Адрес компании"
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact_form.address)
         # Заполнение поля "Домашний телефон"
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home_phone)
+        wd.find_element_by_name("home").send_keys(contact_form.home_phone)
         # Заполнение поля "Мобильный телефон"
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile_phone)
+        wd.find_element_by_name("mobile").send_keys(contact_form.mobile_phone)
         # Заполнение поля "Рабочий телефон"
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work_phone)
+        wd.find_element_by_name("work").send_keys(contact_form.work_phone)
         # Заполнение поля "E-mail"
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(contact_form.email)
         # Заполнение поля "Домашняя страница"
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homepage)
+        wd.find_element_by_name("homepage").send_keys(contact_form.homepage)
         # Выбор дня даты рождения (выпадающий список)
-        if not wd.find_element_by_xpath(option_day_birthday).is_selected():
-            wd.find_element_by_xpath(option_day_birthday).click()
+        if not wd.find_element_by_xpath(contact_form.option_day_birthday).is_selected():
+            wd.find_element_by_xpath(contact_form.option_day_birthday).click()
         # Выбор месяца даты рождения (выпадающий список)
-        if not wd.find_element_by_xpath(option_month_birthday).is_selected():
-            wd.find_element_by_xpath(option_month_birthday).click()
+        if not wd.find_element_by_xpath(contact_form.option_month_birthday).is_selected():
+            wd.find_element_by_xpath(contact_form.option_month_birthday).click()
         # Заполнение поля "Год рождения"
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(year_birthday)
+        wd.find_element_by_name("byear").send_keys(contact_form.year_birthday)
         # Заполнение поля "Домашний адрес"
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(home_address)
+        wd.find_element_by_name("address2").send_keys(contact_form.home_address)
         # Сохранение контакта
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # Авторизация
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
@@ -119,6 +118,7 @@ class test_add_contact(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
