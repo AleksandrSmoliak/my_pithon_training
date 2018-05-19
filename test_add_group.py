b5cf4d94eb986249b08a43d9031cdd6393ef7c79
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-05-19 16:22:31 +0300

    Выделение вспомогательных методов и классов.

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
+from group import Group
 import unittest
 
 
@@ -16,50 +17,56 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.Open_group_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
+        self.return_group_page(wd)
+        self.logout(wd)
+
+    def test_add_group_empty(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.Open_group_page(wd)
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_group_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
-        # Логаут
         wd.find_element_by_link_text("Logout").click()
 
     def return_group_page(self, wd):
-        # Открытие страницы с группасм
         wd.find_element_by_link_text("groups").click()
 
     def Open_group_page(self, wd):
-        # Открытие страницы с группами
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, group):
         # Создание новой группы
         wd.find_element_by_name("new").click()
         # Заполнение полей группы
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Group1")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Group1_Header")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Group1_Footer")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Сохранение создаваемой группы
         wd.find_element_by_name("submit").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
