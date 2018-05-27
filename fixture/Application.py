68a39dc305e36b389e28778e1ebe66f272622222
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-05-27 11:28:36 +0300

    Выделен класс для работвы с группами

--- fixture/Application.py
+++ fixture/Application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -7,35 +8,7 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-    def return_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # Открытие страницы создания группы
-        self.open_group_page()
-        # Создание новой группы
-        wd.find_element_by_name("new").click()
-        # Заполнение полей группы
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Сохранение создаваемой группы
-        wd.find_element_by_name("submit").click()
-        # Возвращение на страницу со списком групп
-        self.return_group_page()
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
---
+++ fixture/group.py
@@ -0,0 +1,32 @@
+class GroupHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def return_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, group):
+        wd = self.app.wd
+        # Открытие страницы создания группы
+        self.open_group_page()
+        # Создание новой группы
+        wd.find_element_by_name("new").click()
+        # Заполнение полей группы
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # Сохранение создаваемой группы
+        wd.find_element_by_name("submit").click()
+        # Возвращение на страницу со списком групп
+        self.return_group_page()
