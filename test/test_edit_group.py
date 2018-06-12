e065014167835570affb2687f97afaffb3517b5a
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-06-12 12:49:48 +0300

    Сравнение списка групп до и после модификации.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,16 +4,25 @@ from model.group import Group
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="Edit_name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)  == len(new_groups)
 
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="Edit_Header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)  == len(new_groups)
 
 
 def test_edit_first_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(footer="Edit_Footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
