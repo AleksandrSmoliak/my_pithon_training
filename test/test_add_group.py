b4898d7837891f0a1ef147b08935c8aed33b7044
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-06-12 14:05:27 +0300

    Добавление проверки сравнения при модификации групп.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,20 +5,32 @@ from model.group import Group
 def test_add_group(app):
     # Получаем текущий список групп
     old_groups = app.group.get_group_list()
+    # В локальную переменную добавляем группу
+    group = Group(name="Group1", header="Group1_Header", footer="Group1_Footer")
     # Добавляем новую группу
-    app.group.create(Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
+    app.group.create(group)
     # Получаем новый список групп
     new_groups = app.group.get_group_list()
     # Сравниваем длинну старого списка групп с новым
     assert len(old_groups) + 1 == len(new_groups)
+    # В переменную добавляем ту же самую группу, что добавили через интерфейс
+    old_groups.append(group)
+    # Проверка сравнения старой и новой группы
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_group_empty(app):
     # Получаем текущий список групп
     old_groups = app.group.get_group_list()
+    # В локальную переменную добавляем группу
+    group = Group(name="", header="", footer="")
     # Добавляем новую пустую группу
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create(group)
     # Получаем новый список групп
     new_groups = app.group.get_group_list()
     # Сравниваем длинну старого списка групп с новым
     assert len(old_groups) + 1 == len(new_groups)
+    # В переменную добавляем ту же самую группу, что добавили через интерфейс
+    old_groups.append(group)
+    # Проверка сравнения старой и новой группы
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,9 +5,13 @@ def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="Edit_name"))
+    group = Group(name="Edit_name")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_first_group_header(app):
