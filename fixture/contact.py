f571c23f9059fbe5b3d53b8ea9f35b438530cb5d
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-06-16 13:44:53 +0300

    Добавлены тысты для сравнения телефонов выведенных нв главной странице с телефонами на страницах редактирования и просмотра.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,5 @@
 from model.contact_class import Contact
-
+import re
 
 class ContactHelper:
     def __init__(self, app):
@@ -95,12 +95,16 @@ class ContactHelper:
     def edit_first_contact(self, Contact):
         self.edit_contact_by_index(Contact, 0)
 
-    def edit_contact_by_index(self, Contact, index):
+    def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
         # Открытие домашней страницы
         self.app.open_home_page()
         # Нажатие кнопки "Edit" для редактирования
         wd.find_elements_by_xpath("//table[@id='maintable']/tbody/tr[@name='entry']/td[8]/a/img")[index].click()
+
+    def edit_contact_by_index(self, Contact, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
         # Заполнение поля "Имя"
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -179,8 +183,38 @@ class ContactHelper:
             self.app.open_home_page()
             self.contact_cache = []
             for element in wd.find_elements_by_name("entry"):
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                lastname = element.find_element_by_xpath(".//td[2]").text
-                self.contact_cache.append(Contact(lastname=lastname, id=id))
+                cells = element.find_elements_by_tag_name("td")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                id = cells[0].find_element_by_name("selected[]").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  home_phone=all_phones[0], mobile_phone=all_phones[1], work_phone=all_phones[2]))
         return list(self.contact_cache)
 
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home_phone = wd.find_element_by_name("home").get_attribute("value")
+        mobile_phone = wd.find_element_by_name("mobile").get_attribute("value")
+        work_phone = wd.find_element_by_name("work").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id, home_phone=home_phone, mobile_phone=mobile_phone, work_phone=work_phone)
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_phone = re.search("H: (.*)", text).group(1)
+        mobile_phone = re.search("M: (.*)", text).group(1)
+        work_phone = re.search("W: (.*)", text).group(1)
+        return Contact(home_phone=home_phone, mobile_phone=mobile_phone, work_phone=work_phone)
