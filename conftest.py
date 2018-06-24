7b9d1f55666bd0930360f9d9f58914df7b092465
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-06-24 10:39:15 +0300

    1. Создние конфигурационного файла 2. Чтение параметров запуска из созданного файла.

--- conftest.py
+++ conftest.py
@@ -1,20 +1,22 @@
 import pytest
+import json
 from fixture.Application import Application
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseURL")
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseURL'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -29,4 +31,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseURL", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
