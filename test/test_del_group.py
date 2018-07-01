644ed10ff0bd826cbcdb805213d98a93b1e2ddb6
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-07-01 11:30:10 +0300

    Создание отключаемой проверки пользовательского интерфейса.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,7 @@ from random import randrange
 import random
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Group1", header="Group1_Header", footer="Group1_Footer"))
     old_groups = db.get_group_list()
@@ -15,3 +15,5 @@ def test_delete_some_group(app, db):
     old_groups.remove(group)
     # Сравниваем старый и новый списки
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
