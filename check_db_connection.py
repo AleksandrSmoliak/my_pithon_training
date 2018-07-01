50ed8025e977d4b343c3a913705b2f9af6d8fe83
AleksandrSmoliak <39195330+AleksandrSmoliak@users.noreply.github.com>
2018-07-01 16:20:07 +0300

    Ошибка при получении списка контактов принадлежащик к определенной группе.

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,9 +1,9 @@
 from fixture.orm import ORMfixture
-
+from model.group import Group
 db = ORMfixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id="82"))
     for item in l:
         print(item)
     print(len(l))
