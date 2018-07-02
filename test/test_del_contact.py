7deab19ea72414523cd6d88b1e0a15af5e0145fe
AleksandrSmoliak <crucis.spb@gmail.com>
2018-07-02 17:51:51 +0300

    Доработка теста удаления контакта. Получение данных из БД. Добавлена задержка перед считыванием новых данных из БД.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,9 +1,10 @@
 from model.contact_class import Contact
 from random import randrange
+import time
 import random
 
 def test_delete_first_contact(app, db, check_ui):
-    if app.contact.contact_count() == 0:
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Александр", middlename="Владимирович", lastname="Смоляк", nickname="Crucis",
                                title="Новый контакт", company="Новая компания", address="Адрес компании",
                                home_phone="+7(812)999-99-99", mobile_phone="+7(911)923-00-99", work_phone="+7(812)777-77-77",
@@ -14,6 +15,8 @@ def test_delete_first_contact(app, db, check_ui):
     old_contact = db.get_contact_list()
     contact = random.choice(old_contact)
     app.contact.delete_contact_by_id(contact.id)
+    # Задержка в 1 секунду (связано с тем что данные не успевают удалится из базы)
+    time.sleep(1)
     new_contact = db.get_contact_list()
     assert len(old_contact) - 1 == len(new_contact)
     # Удаляем из списка случайный элемент
