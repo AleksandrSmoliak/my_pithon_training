a31435d3d06013e20ceb070762b33cb6101572f5
AleksandrSmoliak <crucis.spb@gmail.com>
2018-07-02 15:21:03 +0300

    Доработка теста добавления контакта. Получение данных из БД.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,13 @@
 from model.contact_class import Contact
 
 
-def test_test_add_contact(app, json_contact):
+def test_test_add_contact(app, json_contact, db):
     contact = json_contact
-    old_contact = app.contact.get_contact_list()
+    #old_contact = app.contact.get_contact_list()
+    old_contact = db.get_contact_list()
     app.contact.create(contact)
     assert len(old_contact) + 1 == app.contact.contact_count()
-    new_contact = app.contact.get_contact_list()
+    #new_contact = app.contact.get_contact_list()
+    new_contact = db.get_contact_list()
     old_contact.append(contact)
     assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
