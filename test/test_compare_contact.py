a475bc8d9922477a4ff261add116d05d163fb5b2
AleksandrSmoliak <crucis.spb@gmail.com>
2018-07-05 12:35:35 +0300

    Доработка теста сравнения контактов. Сравнение производится со списком полученным из БД.

--- test/test_compare_contact.py
+++ test/test_compare_contact.py
@@ -1,11 +1,26 @@
 import re
 from random import randrange
+from model.contact_class import Contact
 
-def test_compare_contact(app):
+def test_compare_contact(app, db):
     all_contact = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip(),
+                middlename=contact.middlename.strip(), nickname=contact.nickname.strip(), title=contact.title.strip(),
+                company=contact.company.strip(), address=contact.address.strip(), home_phone=contact.home_phone.strip(),
+                mobile_phone=contact.mobile_phone.strip(), work_phone=contact.work_phone.strip(), email=contact.email.strip(),
+                homepage=contact.homepage.strip())
+    # Получаем список контактов из БД и очищаем от пробелов
+    contacts_db = map(clean, db.get_contact_list())
+    # генерируем случайное значение с ограничением в длинну списка контактов
     index = randrange(len(all_contact))
+    # Получаем значения полей контакта со сгенерированным индексом с главной страницы
     contact_from_home_page = app.contact.get_contact_list()[index]
+    # Получаем значения полей контакта со сгенерированным индексом со страницы редактирования
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    # Сравниваем полный список контактов на главной странице с аналогичным списком из БД
+    assert sorted(all_contact, key=Contact.id_or_max) == sorted(contacts_db, key=Contact.id_or_max)
+    # Сравниваем значения полей главной страницы с полями на странице редактирования
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
