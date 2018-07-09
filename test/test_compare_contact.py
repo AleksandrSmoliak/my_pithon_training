21f58dc57c47afeb2773ab7ea7d35996e8869075
AleksandrSmoliak <crucis.spb@gmail.com>
2018-07-09 13:47:11 +0300

    Исправлеения сравнения контактов с главной страницы и из БД.

--- test/test_compare_contact.py
+++ test/test_compare_contact.py
@@ -3,7 +3,7 @@ from random import randrange
 from model.contact_class import Contact
 
 def test_compare_contact(app, db):
-    all_contact = app.contact.get_contact_list()
+    all_contact = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
     def clean(contact):
         return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip(),
                 middlename=contact.middlename.strip(), nickname=contact.nickname.strip(), title=contact.title.strip(),
@@ -11,34 +11,35 @@ def test_compare_contact(app, db):
                 mobile_phone=contact.mobile_phone.strip(), work_phone=contact.work_phone.strip(), email=contact.email.strip(),
                 homepage=contact.homepage.strip())
     # Получаем список контактов из БД и очищаем от пробелов
-    contacts_db = map(clean, db.get_contact_list())
-    # генерируем случайное значение с ограничением в длинну списка контактов
-    index = randrange(len(all_contact))
+    contacts_db = sorted(map(clean, db.get_contact_list()), key=Contact.id_or_max)
     # Получаем значения полей контакта со сгенерированным индексом с главной страницы
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    # Получаем значения полей контакта со сгенерированным индексом со страницы редактирования
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    # Сравниваем полный список контактов на главной странице с аналогичным списком из БД
-    assert sorted(all_contact, key=Contact.id_or_max) == sorted(contacts_db, key=Contact.id_or_max)
-    # Сравниваем значения полей главной страницы с полями на странице редактирования
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
+    for index in range(0, len(all_contact)-1):
+        assert contact_from_home_page[index].firstname == contacts_db[index].firstname
+        assert contact_from_home_page[index].lastname == contacts_db[index].lastname
+        assert contact_from_home_page[index].address == contacts_db[index].address
+        assert contact_from_home_page[index].all_phones_from_home_page == merge_phones_like_on_home_page(contacts_db[index])
+        assert contact_from_home_page[index].all_email_from_home_page == merge_email_like_on_home_page(contacts_db[index])
 
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear_space(clear_mail(x)),
                                 filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3])))
+                                       [contact.email, contact.email2, contact.email3]))))
 
-def clear(s):
-    return re.sub("[()-]", "", s)
+def clear_space(s):
+    return re.sub("\s+", " ", s)
+
+def clear_mail(s):
+    return re.sub("[() -]", " ", s)
+
+def clear_phones(s):
+    return re.sub("[() -]", "", s)
 
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
+                            map(lambda x: clear_phones(x),
                                 filter(lambda x: x is not None,
                                        [contact.home_phone, contact.mobile_phone, contact.work_phone]))))
