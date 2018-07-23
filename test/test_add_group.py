10cf9890fd13330bc403006a834efa475ad302bc
AleksandrSmoliak <crucis.spb@gmail.com>
2018-07-23 18:02:56 +0300

    Генерация отчетов с помощью Allure.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
 
 
 def test_add_group(app, db, json_groups):
     group = json_groups
-    # Получаем текущий список групп (из БД)
-    old_groups = db.get_group_list()
-    # Добавляем новую группу
-    app.group.create(group)
-    # Получаем новый список групп (из БД)
-    new_groups = db.get_group_list()
-    # В переменную добавляем ту же самую группу, что добавили через интерфейс
-    old_groups.append(group)
-    # Проверка сравнения старой и новой группы
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step('Получаем текущий список групп (из БД)'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('Добавляем новую группу %s ' % group):
+        app.group.create(group)
+    with pytest.allure.step('Получаем новый список групп (из БД)'):
+        new_groups = db.get_group_list()
+    with pytest.allure.step('В переменную добавляем ту же самую группу, что добавили через интерфейс'):
+        old_groups.append(group)
+    with pytest.allure.step('Проверка сравнения старой и новой группы'):
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
